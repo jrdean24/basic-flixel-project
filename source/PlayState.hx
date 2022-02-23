300e9592f3767a0247dfe6ea64b6864cf6d1e97f
Jessica Dean <jrd2017@live.missouristate.edu>
2022-02-22 23:56:35 -0600

    Added and edited text

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,6 +2,8 @@ package;
 
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
 import hero.Hero;
 
 class PlayState extends FlxState
@@ -14,6 +16,11 @@ class PlayState extends FlxState
 
 		hero = new Hero(300, 300);
 		add(hero);
+
+		var text = new flixel.text.FlxText(75, 200, 0, " Hello World", 64);
+		text.setFormat(null, 64, 0x770088, CENTER, OUTLINE, 0xFF00FF00, true);
+		text.borderSize = 5;
+		add(text);
 	}
 
 	override public function update(elapsed:Float)
