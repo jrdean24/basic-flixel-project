6a2e0451a4a9cdc5168939cb27bb9f51d3fe0fff
Sam Bumgardner <Sambumgardner1@gmail.com>
2022-04-26 19:10:26 -0500

    Worked through part 2 of assignment.

--- source/ScreensaverHero.hx
+++ source/ScreensaverHero.hx
@@ -1,17 +1,48 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
 
 class ScreensaverHero extends FlxSprite
 {
-	static inline var SPEED:Float = 200;
+	static inline var MAX_SPEED:Float = 200;
+	static inline var MIN_SPEED:Float = -200;
+
+	var randomX:Float = 0;
+	var randomY:Float = 0;
 
 	public function new()
 	{
 		super(x, y);
-		makeGraphic(25, 25, FlxColor.BLUE, false, "ScreenSaverHero");
-		drag.x = drag.y = 1600;
+		makeGraphic(25, 25, FlxColor.WHITE, false, "ScreenSaverHero");
+		randomizeMovement();
+	}
+
+	private function randomizeMovement()
+	{
+		randomX = FlxG.random.float(MIN_SPEED, MAX_SPEED);
+		randomY = FlxG.random.float(MIN_SPEED, MAX_SPEED);
+		velocity.set(randomX, randomY);
+	}
+
+	private function bounceOffEdge()
+	{
+		if (x < 0) {
+			velocity.x = velocity.x * -1;
+		}
+
+		if (x > FlxG.width - width) {
+			velocity.x = velocity.x * -1;
+		}
+
+		if (y < 0) {
+			velocity.y = velocity.y * -1;
+		}
+
+		if (y > FlxG.height - height) {
+			velocity.y = velocity.y * -1;
+		}
 	}
 
 	override public function update(elapsed:Float)
@@ -26,12 +57,18 @@ class ScreensaverHero extends FlxSprite
 		}
 		else if (this.x < 250 && this.y < 250)
 		{
-			velocity.set(SPEED, 0);
-			// this.color = FlxColor.YELLOW;
+			this.color = FlxColor.YELLOW;
 		}
 		else if (this.x < 250 && this.y > 250)
 		{
 			this.color = FlxColor.BLUE;
+		}
+
+		bounceOffEdge();
+
+		// if you press space, randomize movement
+		if (FlxG.keys.justReleased.SPACE) {
+			randomizeMovement();
 		}
 
 		super.update(elapsed);
